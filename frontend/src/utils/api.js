7a01958060be394957e2bce9d31bb66440117b07
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2026-04-11 14:50:03 +0530

    delete: unused file

--- frontend/src/utils/api.js
+++
@@ -1,3 +0,0 @@
-// @deprecated — Renamed to apiBase.js. This stub will be removed in a future PR.
-// All imports should use "./utils/apiBase.js" instead.
-export { API_BASE, parseJsonResponse } from "./apiBase.js";
